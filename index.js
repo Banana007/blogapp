4a6aa87855f2324ef55b577aeae83a7719429bdd
Eugene <eugenefromrus@gmail.com>
2018-11-10 16:26:18 +1000

    second step complited

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello Worldsss');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const arr = ['tits', 'boobs', 'ass', 'pussy'];
+app.get('/', (req, res) => res.render('index',{arr: arr}));
+
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+  arr.push(req.body.text);
+  res.redirect('/');
+  console.log(req.body)
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
